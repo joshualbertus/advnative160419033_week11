90a810ba05205fd5d721d141c65d076f9e6d43da
Joshua Albertus S <joshuasamarianto@gmail.com>
2022-03-07 21:40:00 +0700

    homework

--- app/src/main/java/id/ubaya/advweek4/view/StudentDetailFragment.kt
+++ app/src/main/java/id/ubaya/advweek4/view/StudentDetailFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import id.ubaya.advweek4.R
+import id.ubaya.advweek4.viewmodel.DetailViewModel
+import id.ubaya.advweek4.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_student_detail.*
+import kotlinx.android.synthetic.main.fragment_student_list.*
 
 class StudentDetailFragment : Fragment() {
+    private lateinit var viewModel: DetailViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -15,4 +23,20 @@ class StudentDetailFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_student_detail, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        observeViewModel()
+    }
+
+   private fun observeViewModel() {
+       viewModel.studentLiveData.observe(viewLifecycleOwner) {
+           editID.setText(it.id)
+           editName.setText(it.name)
+           editDOB.setText(it.dob)
+           editPhone.setText(it.phone)
+       }
+   }
 }
