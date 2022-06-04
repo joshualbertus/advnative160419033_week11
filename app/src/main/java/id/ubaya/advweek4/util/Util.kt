a41fb2b2fa4d83b8adc51a6ce5d5db6cffe178fb
Joshua Albertus S <joshuasamarianto@gmail.com>
2022-06-04 08:18:19 +0700

    init

--- app/src/main/java/id/ubaya/advweek4/util/Util.kt
+++ app/src/main/java/id/ubaya/advweek4/util/Util.kt
@@ -1,5 +1,9 @@
 package id.ubaya.advweek4.util
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
@@ -20,4 +24,18 @@ fun ImageView.loadImage(url: String?, progressBar: ProgressBar) {
 
             override fun onError(e: Exception?) { }
         })
+}
+
+fun createNotificationChannel(context: Context, importance: Int, showBadge: Boolean,
+name: String, description: String) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val channelID = "${context.packageName}-$name"
+        val channel = NotificationChannel(channelID, name, importance).apply {
+            this.description = description
+            setShowBadge(showBadge)
+        }
+
+        val notificationManager = context.getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
+    }
 }
--- app/src/main/java/id/ubaya/advweek4/view/StudentDetailFragment.kt
+++ app/src/main/java/id/ubaya/advweek4/view/StudentDetailFragment.kt
@@ -1,6 +1,7 @@
 package id.ubaya.advweek4.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,9 +12,14 @@ import id.ubaya.advweek4.R
 import id.ubaya.advweek4.util.loadImage
 import id.ubaya.advweek4.viewmodel.DetailViewModel
 import id.ubaya.advweek4.viewmodel.ListViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_student_detail.*
 import kotlinx.android.synthetic.main.fragment_student_list.*
 import kotlinx.android.synthetic.main.student_list_item.view.*
+import java.util.concurrent.TimeUnit
 
 class StudentDetailFragment : Fragment() {
     private lateinit var viewModel: DetailViewModel
@@ -38,11 +44,23 @@ class StudentDetailFragment : Fragment() {
 
    private fun observeViewModel() {
        viewModel.studentLiveData.observe(viewLifecycleOwner) {
-           editID.setText(it.id)
-           editName.setText(it.name)
-           editDOB.setText(it.dob)
-           editPhone.setText(it.phone)
-           imageDetailStudent.loadImage(it.photoURL, progressBarLoadingPhoto)
+           val student = it
+           student?.let {
+               editID.setText(it.id)
+               editName.setText(it.name)
+               editDOB.setText(it.dob)
+               editPhone.setText(it.phone)
+               imageDetailStudent.loadImage(it.photoURL, progressBarLoadingPhoto)
+               buttonNotif.setOnClickListener{
+                   Observable.timer(5, TimeUnit.SECONDS)
+                       .subscribeOn(Schedulers.io())
+                       .observeOn(AndroidSchedulers.mainThread())
+                       .subscribe {
+                           Log.d("mynotif", "Notification delayed after 5 seconds")
+                           MainActivity.showNotification(student.name.toString(), "Notification created", R.drawable.ic_baseline_person_24)
+                       }
+               }
+           }
        }
    }
 }
